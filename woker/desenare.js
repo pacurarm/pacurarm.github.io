fa3e859d1fad2d1907f7a4ecc0d7b24d25840c90
pacurarm <56154025+pacurarm@users.noreply.github.com>
2019-11-25 13:44:52 +0200

    Add files via upload

--- woker/desenare.js
+++ woker/desenare.js
@@ -1,10 +1,11 @@
-document.getElementById("id_logic").innerHTML ="2019.11.25.5";
+document.getElementById("id_logic").innerHTML ="2019.11.25.6";
 document.getElementById("id_start").addEventListener("click", start);
 document.getElementById("id_stop").addEventListener("click", stop);
 
 var timer_id;
 var unghi = {};
 	unghi.valoare = 0;
+var muncitor = null;
 
 function desenare (unghi) 
 {
@@ -41,11 +42,15 @@ function start ()
 	document.getElementById("id_stop").disabled = false;
 	
 	timer_id = setInterval(desenare, 20, unghi);
-	
-	var muncitor = new Worker ("prime.js");
-	muncitor.onmessage = function (e){
+	if (muncitor == null{
+	muncitor = new Worker ("prime.js");
+	muncitor.onmessage = function(e){
 	document.getElementById("id_prime").innerHTML = e.data;
 	}
+	muncitor.postMessage("start");
+	}
+	else 
+		muncitor.postMessage("start");
 }
 
 function stop ()
@@ -54,5 +59,6 @@ function stop ()
 	document.getElementById("id_stop").disabled = true;
 	
 	clearInterval(timer_id);
+	muncitor.postMessage("stop");
 }
 
